30297620afed45a80401b88b51f84c5ddc2186e1
mohammed761-dl <mohammedcherkaoui761@gmail.com>
2023-05-22 16:59:31 +0100

    update

---
+++ alias_management.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+
+
+
+/*
+ * print_alias - add, remove or show aliases
+ * @data: struct for the program's data
+ * @alias: name of the alias to be printed
+ * Return: zero if sucess, or other number if its declared in the arguments
+ * */
+
+int print_alias(data_of_program *data, char *alias)
+{
+	int i, j, alias_length;
+	char buffer[250] = {'\0'};
+
+
+	if (data->alias_list)
+	{
+		alias_length = str_length(alias);
+		for (i = 0; data->alias_list[i]; i++)
+		{
+			if (!alias || (str_compare(data->alias_list[i], alias, alias_lengh) &&   data->alias_list[i][alias_length] == '='))
+			{
+				for (j = 0; data->alias_list[i][j]; j++)
+				{
+					buffer[j] = data->alias_liste[i][j];
+					if (data->alias_list[i][j] == '=')
+						break;
+				}
+				buffer[j + 1] = '\0';
+				buffer_add(buffer, "'");
+				buffer_add(buffer, data->alias_list[i] + j + 1);
+				buffer_add(buffer, "'\n");
+				_print(buffer);
+			}
+		}
+	}
+
+	return(0);
+}
