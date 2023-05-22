ecdd9e464405b3326d7039dbff10735b3d73383a
mohammed761-dl <mohammedcherkaoui761@gmail.com>
2023-05-22 19:44:07 +0100

    update

--- alias_management.c
+++ alias_management.c
@@ -59,3 +59,39 @@ char *get_alias(data_of_program *data, char *name)
 	}
 	return (NULL);
 }
+
+
+
+int set_alias(char *alias_string, data_of_program *data)
+
+{
+	int i, j;
+	char buffer[250] = {'0'}, *temp = NULL;
+
+	if (alias_string == NULL ||  data->alias_list == NULL)
+		return (1);
+	for (i = 0; alias_string[i]; i++)
+		if (alias_string[i] != '=')
+			buffer[i] = alias_string[i];
+		else
+		{
+			temp = get_alias(data, alias_string + i + 1);
+			break;
+		}
+	for (j = 0; data->alias_list[j]; j++)
+		if (str_compare(buffer, data->alias_list[j], i) &&
+			data->alias_list[j][i] == '=')
+		{
+			free(data->alias_list[j]);
+			break;
+		}
+	if (temp)
+	{
+		buffer_add(buffer, "=");
+		buffer_add(buffer, temp);
+		data->alias_list[j] = str_duplicate(buffer);
+	}
+	else
+		data->alias_list[j] = str_duplicate(alias_string);
+	return (0);
+}
