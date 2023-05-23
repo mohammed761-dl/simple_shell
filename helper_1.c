3f45ee839e30f5273c149ea7fbc0a44501f9c5b9
mohammed761-dl <mohammedcherkaoui761@gmail.com>
2023-05-23 18:34:12 +0100

    update

--- helper_1.c
+++ helper_1.c
@@ -1,12 +1,21 @@
-#include "shell.h"
+i#include "shell.h"
 
 
-/**
- * free_recurrent_data - free the fields needed each loop
-	*/
+/*
+ * 
+ *
+ * FREE_RECURRENT_DATA - FREE THE FILEDS NEEDED EACH LOOP
+ *
+ *
+*/
+
+
+
 
 void free_reccurent_data(data_of_program *data)
-	 {
+
+
+{
 		 if(data->input_line)
 			 free(data->input_line);
 		 if(data->command_name)
@@ -18,35 +27,35 @@ void free_reccurent_data(data_of_program *data)
 
 }
 
-/*
- * free_all_data - free all field of the data
- */
 
 void free_all_data(data_of_program *data)
 
-	if (data->file_descriptor != 0)
 {
+	if (data->file_descriptor != 0)
+	{
 		perror(data->program_name);
 
+	}
+
+	free_recurrent_data(data);
+	free_array_of_pointers(data->env);
+	free_array_of_pointers(data->alias_list);
 }
 
-free_recurrent_data(data);
-free_array_of_pointers(data->env);
-free_array_of_pointers(data->alias_list);
-}
 
-/*
- * free_array_of_pointers - frees each pointer of an array of pointers and the arraytoo
- */
 
 void free_array_of_pointers(char **array)
+
+
 {
 	int i;
 
-	if (array != NULL)
-	{
-		for(i = 0; array; i++)
-			free(array[i]);
+		if (array != NULL)
+		{
+			for(i = 0; array; i++)
+				free(array[i]);
 
-		free(array);
-		array = NULL;
+			free(array);
+			array = NULL;
+	}
+}
