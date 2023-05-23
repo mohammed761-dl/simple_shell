e838b8020d4a3f932364dc91dbcd7a059fab5298
mohammed761-dl <mohammedcherkaoui761@gmail.com>
2023-05-23 20:05:28 +0100

    Update

--- _getline.c
+++ _getline.c
@@ -1,17 +1,23 @@
 #include "shell.h"
 
 
-/* aurhors: mohammed cherkaoui & soukaina
- *
- *
- * _getline - read one line from the prompt
- *
- *
- * @data: struct for the program's data
- *
- *
- * Return: reading counting bytes
- */
+/* 
+  aurhors: mohammed cherkaoui & soukaina
+ 
+=>for the first fonction:
+  _getline: this file can read one line from the prompt.
+  data: struct for the program's data
+  Return: ine return he can read and count bytes.
+
+=>for the secand fonction: 
+  check_logic_ops: checks and split for (and/or) operators.
+  array_commands: array of the commands.
+  i: index in the array_commands to be checked.
+  array_operators: array of the logical operators for each previous command.
+  Return: index of the last command in the array_commands.
+
+
+*/
 
 
 
@@ -21,36 +27,62 @@ int _getline(data_of_program *data)
 {
 	
 	char buff[BUFFER_size] = {'\0'};
+	
 	static char *array_commands[10] = {NULL};
+	
 	static char array_operators[10] = {'\0'};
+	
 	ssize_t bytes_read, i = 0;
 
 
 	
 	if (!array_commands[0] || (array_operators[0] == '&' && errno !=0) || (array_operators[0] == '|' && errno==0))
+	
 	{
+	
 		for (i = 0; array_commands[i]; i++)
+	
 		{
+		
 			free(array_commands[i]);
+			
 			array_commands[i] = NULL;
+		
 		}
 
+		
 		bytes_read = read(data->filr_descriptor, &buff, BUFFER_SIZE - 1);
+		
 		if (bytes_read == 0)
+		
 			return (-1);
+
 		i = 0;
-		do{
+		
+		do
+		{
+		
 			array_commands[i] = str_duplicate(_strtok(i ? NULL : buff, "\n;" ) );
+			
 			i = check_logic_ops(array_commands, i, array_operators);
-		}while (array_commands[i++]);
+		
+		}
+	
+		while (array_commands[i++]);
+	
 	}
 
 	data->input_line = array_commands[i + 1];
+	
 	for (i = 0; array_commands[i]; i++)
+	
 	{
+	
 		array_commands[i] = array_commands[i + 1];
+		
 		array_operators[i] = array_operators[i + 1]; 
 	}
+
 }
 
 
@@ -62,34 +94,65 @@ int _getline(data_of_program *data)
 
 
 int check_logic_ops(char *array_commands[], int i, char array_operators[])
+
 {
+
 	char *temp = NULL;
+	
 	int j;
 
+
+	
 	for (j = 0; array_commands[i] != NULL && array_commands[i][j]; j++)
+	
 	{
+	
 		if (array_commands[i][j] == '&' && array_commands[i][j + 1] == '&')
+		
 		{
+		
 			temp = array_commands[i];
+			
 			array_commands[i][j] = '\0';
+			
 			array_commands[i] = str_duplicate(array_commands[i]);
+			
 			array_commands[i+1] = str_duplicate(temp + j + 2);
+			
 			i++;
+			
 			array_operators[i] = '&';
+			
 			free(temp);
+			
 			j = 0;
 		}
+
+		
 		if (array_commands[i][j] == '|' && array_commands[i][j + 1] == '|')
+		
 		{
+		
 			temp = array_commands[i];
+			
 			array_commands[i][j] = '\0';
+			
 			array_commands[i] = str_duplicate(array_commands[i]);
+			
 			array_commands[i + 1] = str_duplicate(temp + j + 2);
+			
 			i++;
+			
 			array_operators[i] = '|';
+			
 			free(temp);
+			
 			j = 0;
 		}
+
 	}
+
+	
 	return (i);
 }
+
