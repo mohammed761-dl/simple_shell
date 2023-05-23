5f843b5ec004ab25aaa673ca0d405ccc1e956fa6
mohammed761-dl <mohammedcherkaoui761@gmail.com>
2023-05-23 19:44:17 +0100

    update

--- shell.h
+++ shell.h
@@ -106,8 +106,73 @@ int find_program(data_of_program *data);
 
 
 
-// helpers_free.c
+// helpers_free.c .
 
 void free_array_of_pointers(char **directories);
 void free_recurrent_data(data_of_program *data);
 void free_all_data(data_of_program *data);
+
+
+
+// builtins_more.c .
+
+int builtin_exit(data_of_program *data);
+int builtin_cd(data_of_program *data);
+int set_work_directory(data_of_program *data, char *new_dir);
+int builtin_help(data_of_program *data);
+int builtin_alias(data_of_program *data);
+
+
+
+//builtins_env.c .
+
+int builtin_env(data_of_program *data);
+int builtin_set_env(data_of_program *data);
+int builtin_unset_env(data_of_program *data);
+
+
+
+//env_management.c .
+
+char *env_get_key(char *name, data_of_program *data);
+int env_set_key(char *key, char *value, data_of_program *data);
+int env_remove_key(char *key, data_of_program *data);
+void print_environ(data_of_program *data);
+
+
+
+//helpers_print.c .
+
+int _print(char *string);
+int _printe(char *string);
+int _print_error(int errorcode, data_of_program *data);
+
+
+
+//helpers_string.c .
+
+int str_length(char *string);
+char *str_duplicate(char *string);
+int str_compare(char *string1, char *string2, int number);
+char *str_concat(char *string1, char *string2);
+void str_reverse(char *string);
+
+
+
+// helpers_numbers.c .
+
+void long_to_string(long number, char *string, int base);
+int _atoi(char *s);
+int count_characters(char *string, char *character);
+
+
+
+//  alias_management.c .
+
+int print_alias(data_of_program *data, char *alias);
+char *get_alias(data_of_program *data, char *alias);
+int set_alias(char *alias_string, data_of_program *data);
+
+
+
+#endif
