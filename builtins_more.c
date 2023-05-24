bc3f13f1c9334d7b0a3bd508f81dc21dbbf1e82d
mohammed761-dl <mohammedcherkaoui761@gmail.com>
2023-05-24 05:55:46 +0100

    Update

--- builtins_more.c
+++ builtins_more.c
@@ -94,119 +94,66 @@ int set_work_directory(data_of_program *data, char *new_dir)
  * @data: struct for the program's data
  * Return: zero if sucess, or other number if its declared in the arguments
  */
-
-
 int builtin_help(data_of_program *data)
-
 {
-
-	
 	int i, length = 0;
-	
 	char *mensajes[6] = {NULL};
 
-
-	
 	mensajes[0] = HELP_MSG;
 
-
-
+	/* validate args */
 	if (data->tokens[1] == NULL)
-	
-
 	{
-	
 		_print(mensajes[0] + 6);
-		
 		return (1);
 	}
-
-	
 	if (data->tokens[2] != NULL)
-	
 	{
-	
 		errno = E2BIG;
-		
 		perror(data->command_name);
-		
 		return (5);
 	}
-
-	
 	mensajes[1] = HELP_EXIT_MSG;
-	
 	mensajes[2] = HELP_ENV_MSG;
-	
 	mensajes[3] = HELP_SETENV_MSG;
-	
 	mensajes[4] = HELP_UNSETENV_MSG;
-	
 	mensajes[5] = HELP_CD_MSG;
 
-
-	
 	for (i = 0; mensajes[i]; i++)
-	
 	{
-	
 		length = str_length(data->tokens[1]);
-		
-		
 		if (str_compare(data->tokens[1], mensajes[i], length))
-		
 		{
-		
 			_print(mensajes[i] + length + 1);
-			
 			return (1);
 		}
-
 	}
-
-	
-
-	
+	/*if there is no match, print error and return -1 */
 	errno = EINVAL;
-	
 	perror(data->command_name);
-	
 	return (0);
 }
 
-
+/**
+ * builtin_alias - add, remove or show aliases
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
 int builtin_alias(data_of_program *data)
-
 {
-
 	int i = 0;
 
-
-	
-
-	
+	/* if there are no arguments, print all environment */
 	if (data->tokens[1] == NULL)
-	
 		return (print_alias(data, NULL));
 
-
-	
 	while (data->tokens[++i])
-	
-	{
-	
+	{/* if there are arguments, set or print each env variable*/
 		if (count_characters(data->tokens[i], "="))
-		
 			set_alias(data->tokens[i], data);
-		
 		else
-
 			print_alias(data, data->tokens[i]);
-	
 	}
 
-
-	
 	return (0);
 }
-
--- str_token.c
+++ str_token.c
@@ -1,24 +1,24 @@
 #include "shell.h"
-/*
+/**
  * _strtok - separates strings with delimiters
- */
-
+ * @line: It´s pointer to array we receive in getline.
+ * @delim: It´s characters we mark off string in parts.
+ * Return: A pointer to the created token
+*/
 char *_strtok(char *line, char *delim)
 {
 	int j;
 	static char *str;
 	char *copystr;
 
-	if (line !=NULL)
-
-             str = line;
-
-	for (; *str  != '\0' ; str++)
+	if (line != NULL)
+		str = line;
+	for (; *str != '\0'; str++)
 	{
-		for (j -0; delim[j] ='\0'; j++)
+		for (j = 0; delim[j] != '\0'; j++)
 		{
 			if (*str == delim[j])
-				break;
+			break;
 		}
 		if (delim[j] == '\0')
 			break;
@@ -40,5 +40,3 @@ char *_strtok(char *line, char *delim)
 	}
 	return (copystr);
 }
-
-
